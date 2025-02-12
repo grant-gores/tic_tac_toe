118497b58280c4c9c08717e34715d2091809f94c
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-02-11 19:40:35 -0600

    Create Player class
    Wrote the logic for the player class. Initializes
    the player and gets the symbol ('X' or 'O').

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,16 @@
 class Player
-  attr_reader :symbol
-
+  attr_accessor :symbol
   def initialize(symbol)
     @symbol = symbol
+  end
+
+  def get_move
+    loop do
+      print "Player #{symbol}, enter your move (row, col): "
+      input = gets.chomp.split(",").map(&:to_i)
+      return input if input.size == 2 && input.all? {|n| n.between(0,2)}
+
+      puts "Invalid input! Please enter two numbers between 0 and 2 separated by a comma."
+    end
   end
 end
